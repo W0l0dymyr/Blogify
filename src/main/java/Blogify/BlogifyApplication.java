e7fcdcdb87b98b0226b1a9e5e8979d1b8447cac6
W0l0dymyr <DjVovic@gmail.com>
2023-02-19 17:27:25 +0200

    Starting of the project

---
+++ src/main/java/Blogify/BlogifyApplication.java
@@ -0,0 +1,13 @@
+package Blogify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogifyApplication.class, args);
+	}
+
+}
